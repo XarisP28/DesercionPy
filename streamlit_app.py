1420700614e0648d8a776312b2801baf38b7309b
XarisP28 <97067537+XarisP28@users.noreply.github.com>
2025-05-13 15:38:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,7 @@ if modelo:
         st.write("Columnas disponibles en el dataset:", df.columns.tolist())
 
         # Aquí selecciona las features que existan en tu CSV
-       features_a_usar = ['Facultad', 'Carrera', 'Sexo', 'Pais', 'Estado', 'Tipo',
-    'Desc. Becas', 'Donativos', 'Deuda Actual', 'TPT',
-    'N° CP', 'N° BA']
+       features_a_usar = ['Facultad', 'Carrera', 'Sexo', 'Pais', 'Estado', 'Tipo','Desc. Becas', 'Donativos', 'Deuda Actual', 'TPT','N° CP', 'N° BA']
 
         # Validar qué columnas faltan para evitar el KeyError
         columnas_faltantes = [col for col in features_a_usar if col not in df.columns]
